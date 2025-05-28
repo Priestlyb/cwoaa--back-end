a6f190aca6adce04c60123d4dd10121d508896b7
Priestly Patrick bassey <pbassey30@gmail.com>
2025-05-28 22:46:08 +0100

    Changed Orgin url on app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // http://localhost:3000 https://thisispriestly.vercel.app
+    origin: "https://cwoaa.vercel.app/", // http://localhost:3000 
     credentials: true,
   })
 );
